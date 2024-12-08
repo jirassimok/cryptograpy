39215da000dd2939b98326ccc09d2829c92b1cbd
Jacob Komissar <jik@bu.edu>
2024-12-08 15:50:07 -0500

    Update module init to import key submodules

--- src/homework/__init__.py
+++ src/homework/__init__.py
@@ -2,10 +2,23 @@ __all__ = (
     'elgamal',
     'euclid',
     'factor',
-    'factors',
     'fastexp',
     'homework4',
-    'prime',
     'pseudoprime',
     'pseudorandom',
+    'randprime',
 )
+
+from . import (
+    elgamal,
+    euclid,
+    factor,
+    fastexp,
+    homework4,
+    pseudoprime,
+    pseudorandom,
+    randprime,
+)
+
+primitive_root = homework4.primitive_root
+discrete_log = homework4.discrete_log
