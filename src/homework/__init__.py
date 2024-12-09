e82ca3b9f5a9841860ea83ac8d85c626e0721aa1
Jacob Komissar <jik@bu.edu>
2024-12-09 02:20:51 -0500

    Update exports and documentation in __init__

--- src/homework/__init__.py
+++ src/homework/__init__.py
@@ -1,3 +1,32 @@
+"""CS789 Project package
+
+This package contains a number of modules.
+
+Core Modules
+-------
+elgamal  - ElGamal encryption and cracking functions
+rsa      - RSA encryption and cracking functions
+euclid   - Euclidean algorithm and family
+factor   - Fast probabilistic factorization algorithms
+fastexp  - Exponentiation by squaring
+homework4    - primitive root generation and discrete logarithms
+pseudoprime  - Miller-Rabin primality testing
+pseudorandom - PRNG implementations
+
+Support Modules
+---------------
+sieve   - A sieve of Eratosthenes
+bitier  - Iterators over bits and the abstract classes for the PRNG classes
+util    - Miscellaneous support functions.
+          This module includes the VERBOSE constant, which, if set
+          to true, will make some of the algorithms in this package
+          print intermediate work even when not requested explicitly.
+randprime   - Prime generation for initializing other PRNGs
+cache_util  - Memoization utilities
+bit_class   - A class representing a single bit (primarily for type-checking
+              purposes)
+"""
+
 __all__ = (
     'elgamal',
     'euclid',
@@ -7,6 +36,29 @@ __all__ = (
     'pseudoprime',
     'pseudorandom',
     'randprime',
+    # Re-exported functions
+    'ElGamal',
+    'crack_elgamal',
+    'gcd',
+    'ext_euclid',
+    'pow',
+    'find_factor_rho',
+    'find_factor_pm1',
+    'factors',
+    'unique_factors',
+    'primitive_root',
+    'is_primitive_root',
+    'discrete_log',
+    'strong_prime_test',
+    'is_prime',
+    'blum_blum_shub',
+    'BlumBlumShub',
+    'naor_reingold',
+    'NaorReingold',
+    'random_prime',
+    'random_prime_3mod4',
+    'system_random_prime',
+    'system_random_prime_3mod4',
 )
 
 from . import (
@@ -20,5 +72,26 @@ from . import (
     randprime,
 )
 
-primitive_root = homework4.primitive_root
-discrete_log = homework4.discrete_log
+from .elgamal import ElGamal, crack as crack_elgamal
+from .euclid import gcd, ext_euclid
+from .fastexp import pow
+from .factor import (
+    find_factor_rho,
+    find_factor_pm1,
+    factors,
+    unique_factors,
+)
+from .homework4 import primitive_root, is_primitive_root, discrete_log
+from .pseudoprime import strong_prime_test, is_prime
+from .pseudorandom import (
+    blum_blum_shub,
+    BlumBlumShub,
+    naor_reingold,
+    NaorReingold,
+)
+from .randprime import (
+    random_prime,
+    random_prime_3mod4,
+    system_random_prime,
+    system_random_prime_3mod4,
+)
